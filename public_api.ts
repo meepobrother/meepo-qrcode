bdfe273301fdac76dc3feb6c4d294d26d796c59f
15138730370 <1037483576@qq.com>
2018-02-16 20:58:56 +0800

    first commit

---
+++ public_api.ts
@@ -0,0 +1,9 @@
+import { NgxQRCodeComponent } from './qrcode.component';
+import { NgModule } from '@angular/core';
+@NgModule({
+    imports: [],
+    exports: [NgxQRCodeComponent],
+    declarations: [NgxQRCodeComponent],
+    providers: [],
+})
+export class QrcodeModule { }
